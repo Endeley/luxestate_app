b1fafa5fcc5509940099bf05ecaf0d395a9afaac
Endeley <93675367+Endeley@users.noreply.github.com>
2023-11-07 03:11:55 +0100

    complete profile updat functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,14 +2,18 @@ import { useRef, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
+import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 function Profile() {
     const fileRef = useRef(null);
-    const { currentUser } = useSelector((state) => state.user);
+    const { currentUser, loading, error } = useSelector((state) => state.user);
     const [file, setFile] = useState(undefined);
     const [filePercentage, setFilePercentage] = useState(0);
     const [fileUploadError, setFileUploadError] = useState(false);
     const [formData, setFormData] = useState({});
+    const [updateSucess, setUpdateSuccess] = useState(false);
+    const dispatch = useDispatch();
 
     /* 
 ======firebase storage =======
@@ -47,11 +51,36 @@ function Profile() {
             }
         );
     };
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value });
+    };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            dispatch(updateUserStart());
+            const res = await fetch(`/server/user/update/${currentUser._id}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+            if (data.success === false) {
+                dispatch(updateUserFailure(data.message));
+                return;
+            }
+            dispatch(updateUserSuccess(data));
+            setUpdateSuccess(true);
+        } catch (error) {
+            dispatch(updateUserFailure(error.message));
+        }
+    };
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text3xl font-semibold text-center my-7'>Profile</h1>
-            <form className='flex flex-col gap-6 '>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-6 '>
                 <input onChange={(e) => setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='image/*' />
                 <img onClick={() => fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt='profile' className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
                 <p className='text-sm self-center'>
@@ -65,15 +94,19 @@ function Profile() {
                         ''
                     )}
                 </p>
-                <input type='text' placeholder='username' id='username' className='border p-3 rounded-lg text-center tracking-widest' />
-                <input type='email' placeholder='email' id='email' className='border p-3 rounded-lg text-center tracking-widest' />
-                <input type='text' placeholder='password' id='password' className='border p-3 rounded-lg text-center tracking-widest' />
-                <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-90 disabled:opacity-80'>update</button>
+                <input type='text' placeholder='username' defaultValue={currentUser.username} id='username' className='border p-3 rounded-lg text-center tracking-widest' onChange={handleChange} />
+                <input type='email' placeholder='email' defaultValue={currentUser.email} id='email' className='border p-3 rounded-lg text-center tracking-widest' onChange={handleChange} />
+                <input type='password' placeholder='password' id='password' className='border p-3 rounded-lg text-center tracking-widest' onChange={handleChange} />
+                <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-90 disabled:opacity-80'>
+                    {loading ? 'Loading...' : 'update'}
+                </button>
             </form>
             <div className='flex justify-between mt-6'>
                 <span className='text-red-700 cursor-pointer'>Delete Account</span>
                 <span className='text-red-700 cursor-pointer'>Sign Out</span>
             </div>
+            <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+            <p className='text-green-700 mt-5'>{updateSucess ? 'User updated successfully' : ''}</p>
         </div>
     );
 }
--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -4,7 +4,7 @@ import { errorHandler } from './error.js';
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return next(errorHandler(403, 'Forbidden Request'));
+        if (err) return next(errorHandler(403, 'Unauthorized Request'));
         req.user = user;
         next();
     });
